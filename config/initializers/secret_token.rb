b8a2ef93335758f9d229a38a06387feddba13c70
Moses Medin <mmedin1@terpmail.umd.edu>
2012-10-15 13:54:57 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Myweb::Application.config.secret_token = '95f5f58f018ffff7bc26d32b522032f29563630a734ca2d7eb12750e36efa90f37ff2fede0937681661a5239a1d1ec0b8bb67f906f3d18a3254271b46a072b97'
